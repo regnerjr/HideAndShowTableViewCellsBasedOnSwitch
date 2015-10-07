96368317bd681d1ec21aae69f782a0aeb71e2194
John Regner <regnerjr@gmail.com>
2015-10-06 22:26:53 -0700

    initial Commit

---
+++ TableViewCellInsertAnimation/LabelsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LabelsTableViewCell.swift
+//  TableViewCellInsertAnimation
+//
+//  Created by John Regner on 10/6/15.
+//  Copyright © 2015 johnregner. All rights reserved.
+//
+
+import UIKit
+
+class LabelsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TableViewCellInsertAnimation/SwitchedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchedTableViewCell.swift
+//  TableViewCellInsertAnimation
+//
+//  Created by John Regner on 10/6/15.
+//  Copyright © 2015 johnregner. All rights reserved.
+//
+
+import UIKit
+
+class SwitchedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
